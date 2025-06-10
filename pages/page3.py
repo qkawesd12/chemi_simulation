0b5046de865474d78528bb33ee8ffa2e58f40d2d
qkawesd12 <qkawesd12@gmail.com>
2025-06-10 18:51:25 +0900

    Update page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -1,8 +1,8 @@
 import streamlit as st
 st.title("📊 몰 수 / 농도 계산기")
 
-moles = st.number_input("물질의 몰 수 (mol)", min_value=0.0, step=0.01)
-volume = st.number_input("용액 부피 (L)", min_value=0.0, step=0.01)
+moles = st.number_input("물질의 몰 수 (mol)", min_value=0.0, step=0.01) #사용자로부터 물질의 몰 수 입력 받기
+volume = st.number_input("용액 부피 (L)", min_value=0.0, step=0.01) #사용자로부터 용액의 부피 입력 받기
 
 if st.button("농도 계산"):
   if volume == 0:
