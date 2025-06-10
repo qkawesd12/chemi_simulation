f25fc949cb223e0c0691acf41c309487b782787e
qkawesd12 <qkawesd12@gmail.com>
2025-06-10 18:57:14 +0900

    Update page5.py

--- pages/page5.py
+++ pages/page5.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.title("🧬 침전 반응")
 
+#침전 반응 예시
 reactions = {
 "AgNO₃ + NaCl": ("AgCl↓ + NaNO₃", "흰색 침전"),
 "BaCl₂ + Na₂SO₄": ("BaSO₄↓ + NaCl", "흰색 침전"),
@@ -13,5 +14,5 @@ selected = st.selectbox("반응 선택", list(reactions.keys()))
 if st.button("침전 확인"):
   result, color = reactions[selected]
   st.subheader("🧬 생성 반응")
-  st.write(result)
-  st.write(f"→ 침전 색: **{color}**")
+  st.write(result) #생성된 반응식 출력
+  st.write(f"→ 침전 색: **{color}**") #침전 색상 출력
